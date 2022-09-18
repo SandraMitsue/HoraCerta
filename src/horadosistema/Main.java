f01569980ad605057aaba94de255bf576d659443
Sandra Cristina Mitsue <sandracm7203@gmail.com>
2022-09-18 15:27:42 -0300

    Initial commit

---
+++ src/horadosistema/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é:\n");
+        System.out.println(relogio);
+    }
+}
